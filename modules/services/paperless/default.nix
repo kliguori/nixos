c5a94b9cf957025b576357946c938fed33b73f69
kevin <liguori.km@gmail.com>
2026-02-22 22:13:04 -0500

    trying manual postgreSQL with paperless

--- modules/services/paperless/default.nix
+++ modules/services/paperless/default.nix
@@ -4,22 +4,17 @@ let
   nginx = config.systemOptions.services.nginx;
   pg = config.systemOptions.services.postgresql;
   tls = config.systemOptions.tls;
-
   host = "paperless.${nginx.baseDomain}";
-
-  dbName = "paperless";
-  dbUser = "paperless";
-
-  sopsFile = ../../../secrets/secrets.yaml;
+  sopsFile = ../../secrets/secrets.yaml;
 in
 {
   options.systemOptions.services.paperless = {
     enable = lib.mkEnableOption "Paperless-ngx document manager";
-
+    
     dataDir = lib.mkOption {
       type = lib.types.path;
       default = "/var/lib/paperless";
-      description = "Directory to store Paperless data (must be persistent).";
+      description = "Persistent directory for Paperless (dataDir, mediaDir, consumeDir).";
     };
   };
 
@@ -39,69 +34,83 @@ in
       }
     ];
 
-    sops.secrets.paperlessSecretKey = {
-      inherit sopsFile;
-      key = "apps/paperless/secretKey";
-      owner = "paperless";
-      group = "paperless";
-      mode = "0400";
-    };
+    sops = {
+      secrets = {
+        paperlessSecretKey = {
+          inherit sopsFile;
+          key = "apps/paperless/secretKey";
+          owner = "paperless";
+          group = "paperless";
+          mode = "0400";
+        };
 
-    sops.secrets.paperlessAdminPassword = {
-      inherit sopsFile;
-      key = "apps/paperless/adminPassword";
-      owner = "paperless";
-      group = "paperless";
-      mode = "0400";
-    };
-
-    sops.templates.paperless-env = {
-      owner = "paperless";
-      group = "paperless";
-      mode = "0400";
-      content = ''
-        PAPERLESS_SECRET_KEY=${config.sops.placeholder."paperlessSecretKey"}
-      '';
-    };
-
-    services = {
-      paperless = {
-        enable = true;
-        address = "127.0.0.1";
-        port = 28981;
-        
-        user = "admin";
-        passwordFile = config.sops.secrets.paperlessAdminPassword.path;
-        
-        dataDir = toString cfg.dataDir;
-        mediaDir = "${toString cfg.dataDir}/media";
-        consumptionDir = "${toString cfg.dataDir}/consume";
-
-        database.createLocally = true;
-
-        environmentFile = config.sops.templates.paperless-env.path;
-
-        settings = {
-          PAPERLESS_URL = "https://${host}"; 
-          PAPERLESS_CONSUMER_RECURSIVE = true; 
-          PAPERLESS_CONSUMER_SUBDIRS_AS_TAGS = true; 
+        paperlessAdminPassword = {
+          inherit sopsFile;
+          key = "apps/paperless/adminPassword";
+          owner = "root";
+          group = "root";
+          mode = "0400";
         };
       };
 
+      templates.paperless-env = {
+        owner = "paperless";
+        group = "paperless";
+        mode = "0400";
+        content = ''
+          PAPERLESS_SECRET_KEY=${config.sops.placeholder."paperlessSecretKey"}
+        '';
+      };
+    };
+
+    systemd = {
+      tmpfiles.rules = [
+        "d ${toString cfg.dataDir} 0750 paperless paperless - -"
+        "d ${toString cfg.dataDir}/media 0750 paperless paperless - -"
+        "d ${toString cfg.dataDir}/consume 0770 paperless paperless - -"
+      ];
+
+      # services.paperless-scheduler.preStart = lib.mkBefore ''
+      #   ${config.services.paperless.package}/bin/paperless-ngx migrate
+      # '';
+    };
+
+    services = {
       postgresql = {
-        ensureDatabases = [ dbName ];
+        ensureDatabases = [ "paperless" ];
         ensureUsers = [
           {
-            name = dbUser;
+            name = "paperless";
             ensureDBOwnership = true;
           }
         ];
       };
 
+      paperless = {
+        enable = true;
+        address = "127.0.0.1";
+        port = 28981;
+        dataDir = toString cfg.dataDir;
+        mediaDir = "${toString cfg.dataDir}/media";
+        consumptionDir = "${toString cfg.dataDir}/consume";
+        consumptionDirIsPublic = true;
+        database.createLocally = false;
+        environmentFile = config.sops.templates.paperless-env.path;
+        passwordFile = config.sops.secrets.paperlessAdminPassword.path;
+        settings = {
+          PAPERLESS_URL = "https://${host}";
+          PAPERLESS_CONSUMER_RECURSIVE = true;
+          PAPERLESS_CONSUMER_SUBDIRS_AS_TAGS = true;
+          PAPERLESS_DBENGINE = "postgresql";
+          PAPERLESS_DBHOST = "/run/postgresql";
+          PAPERLESS_DBNAME = "paperless";
+          PAPERLESS_DBUSER = "paperless";
+        };
+      };
+
       nginx.virtualHosts."${host}" = {
         useACMEHost = nginx.baseDomain;
         forceSSL = true;
-
         locations."/" = {
           proxyPass = "http://127.0.0.1:28981";
           proxyWebsockets = true;
