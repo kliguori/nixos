4a2a720e01574ff151663e1aeed41c3393808813
kevin <liguori.km@gmail.com>
2026-02-23 01:22:12 -0500

    updated bitwarden's weird extension tag

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -78,7 +78,7 @@ in
             private_browsing = true;
 
           };
-          "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
             installation_mode = "force_installed";
             private_browsing = true;
