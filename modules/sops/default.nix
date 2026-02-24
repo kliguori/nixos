04e956456f94dce03b8c84393ca693e98d1ef1fd
kevin <liguori.km@gmail.com>
2026-02-24 09:20:03 -0500

    make sops opt-in

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -1,22 +1,23 @@
 { config, lib, ... }:
 let
+  cfg = config.systemOptions.sops-nix;
   persist = config.systemOptions.impermanence.enable;
   baseDir = "/var/lib/sops-nix";
 
-  keyFile = 
-    if persist 
-    then "/persist${baseDir}/key.txt" 
-    else "${baseDir}/key.txt"; 
+  keyFile = if persist then "/persist${baseDir}/key.txt" else "${baseDir}/key.txt";
 in
 {
-  sops = {
-    age = {
-      inherit keyFile; 
-      generateKey = true;
-      sshKeyPaths = lib.mkForce [ ];
+  options.systemOptions.sops-nix.enable = lib.mkEnableOption "Enable SOPS-nix";
+  config = lib.mkIf cfg.enable {
+    sops = {
+      age = {
+        inherit keyFile;
+        generateKey = true;
+        sshKeyPaths = lib.mkForce [ ];
+      };
+      gnupg.sshKeyPaths = lib.mkForce [ ];
     };
-    gnupg.sshKeyPaths = lib.mkForce [ ];
-  };
 
-  environment.persistence."/persist".directories = lib.mkIf persist [ baseDir ];
+    environment.persistence."/persist".directories = lib.mkIf persist [ baseDir ];
+  };
 }
