4168a99e037b88320b4569127a9bd9ba58445c2e
kevin <liguori.km@gmail.com>
2026-02-24 09:25:12 -0500

    removed sops from kevin

--- users/kevin/default.nix
+++ users/kevin/default.nix
@@ -7,17 +7,17 @@
 }:
 let
   enabled = lib.elem "kevin" config.systemOptions.users;
-  sopsFile = ../../secrets/secrets.yaml;
+  # sopsFile = ../../secrets/secrets.yaml;
 in
 lib.mkIf enabled {
-  sops.secrets.kevinPassword = {
-    inherit sopsFile;
-    neededForUsers = true;
-    key = "users/kevin/hashedPassword";
-    owner = "root";
-    group = "root";
-    mode = "0400";
-  };
+    #  sops.secrets.kevinPassword = {
+    # inherit sopsFile;
+    # neededForUsers = true;
+    # key = "users/kevin/hashedPassword";
+    # owner = "root";
+    # group = "root";
+    # mode = "0400";
+    # };
 
   users.users.kevin = {
     isNormalUser = true;
