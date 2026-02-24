6e040d79f2e16394545d0fd9e4ed533602d5d8f6
kevin <liguori.km@gmail.com>
2026-02-24 15:06:04 -0500

    added ssh keys to admin

--- users/admin/default.nix
+++ users/admin/default.nix
@@ -17,9 +17,8 @@ lib.mkIf enabled {
       "networkmanager"
     ];
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKjOZvkhZPv1wkLTfC+3A1PqVcAEa6svStem0QCT7PoQ kevin@sherlock"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbReMwC07CMv2Mv3ICEvoNrGs1sSDaNZWbGg6cBZ/dh lestrade"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEtVjlECPNj4BXiThBDx6Wx7BAmN+eR9EYacXU+4Ox9H kevin@sherlock"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOavirFl6Xk3GR2bFfGzX28RYqfwld5lnBdSjTTCAV/0 kevin@macbook"
     ];
   };
 }
