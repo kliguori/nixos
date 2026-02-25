f42e2ee8f12c08129d9b117a2869318d1cb41f91
kevin <liguori.km@gmail.com>
2026-02-24 19:07:14 -0500

    adding printer to paperless group

--- users/printer/default.nix
+++ users/printer/default.nix
@@ -1,9 +1,18 @@
-{ pkgs, ... }:
 {
-  # Note the samba password has to be created manually... 
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  paperless = config.systemOptions.services.paperless;
+in
+{
+  # Note the samba password has to be created manually...
   # sudo smbpasswd -a printer
   users.users.printer = {
     isSystemUser = true;
+    extraGroups = lib.optional paperless.enable "paperless";
     description = "SMB user for printer";
     group = "printer";
     createHome = false;
