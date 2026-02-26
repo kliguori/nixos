7d9d860915c918c736f50bd685634d56c2a0133a
kevin <liguori.km@gmail.com>
2026-02-26 09:32:25 -0500

    updated mycroft keys

--- users/admin/default.nix
+++ users/admin/default.nix
@@ -18,7 +18,7 @@ lib.mkIf enabled {
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKjOZvkhZPv1wkLTfC+3A1PqVcAEa6svStem0QCT7PoQ kevin@sherlock"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbReMwC07CMv2Mv3ICEvoNrGs1sSDaNZWbGg6cBZ/dh lestrade"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnHto0uafEodToMmanowEudOxEqGiTqKcJnGXL9g+xy kevin@lestrade"
     ];
   };
 }
