62e4c24842a2a94883386afd831975cdee8e5d10
kevin <liguori.km@gmail.com>
2026-03-02 15:22:36 -0500

    removed bitwarden extension and add desktop app

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -22,6 +22,7 @@ in
 
     environment = {
       systemPackages = with pkgs; [
+        bitwarden-desktop
         kitty
         alacritty
       ];
--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -78,11 +78,11 @@ in
             private_browsing = true;
 
           };
-          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-            private_browsing = true;
-          };
+          # "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          #   installation_mode = "force_installed";
+          #   private_browsing = true;
+          # };
         };
 
         Preferences = {
